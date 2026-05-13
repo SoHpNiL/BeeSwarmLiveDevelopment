20c7e82e1f543f0a841ef249befd17e9f21ac7e0
Shopnil Rahman <shopnil.rhm@gmail.com>
2026-05-14 11:09:35 +1200

    Initial commit from Create Next App

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import Image from "next/image";
 
-export default function Home() {
+import { getHomepageStats } from "./components/homepageStats"
+
+export default async function Home() {
+  const stats = await getHomepageStats()
+
   return (
-
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
       {/* Title */}
       <h1 className="text-2xl font-medium text tracking-tight mb-1">
@@ -24,14 +26,10 @@ export default function Home() {
         </button>
       </div>
 
-      {/* Stats Grid */}
+      {/* Stats Grid from homepageStats */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-8 w-full max-w-4xl">
-        {[
-          { label: 'Active Players', value: '1,284', icon: '📍' },
-          { label: 'Honey Produced', value: '42.8 GB', icon: '🍯' },
-          { label: 'Disocrd Servers', value: '30+', icon: '✨' }
-        ].map((stat, i) => (
-          <div key={i} className="stats shadow  border-2   border-amber-600/20 bg-white/60 backdrop-blur-md rounded-2xl">
+        {stats.map((stat, i) => (
+          <div key={i} className="stats shadow border-2 border-amber-600/20 bg-white/60 backdrop-blur-md rounded-2xl">
             <div className="stat">
               <div className="stat-figure text-amber-600 text-2xl">{stat.icon}</div>
               <div className="stat-title text-amber-900/60 font-bold">{stat.label}</div>
@@ -42,7 +40,7 @@ export default function Home() {
 
         {/*Google Authentication Button TODO*/}
         <div className="absolute top-5 right-10 z-50">
-          <button className="btn rounded-xl bg-white text-black border-2 border-amber-600">
+          <button className="btn rounded-xl bg-white text-black border-2 border-amber-600 hover:scale-110 transition-transform">
             <svg aria-label="Google logo" width="16" height="16" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
             Sign in with Google
           </button>
