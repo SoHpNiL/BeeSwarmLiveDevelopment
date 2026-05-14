3eeb1a9db98268cf316b865be1a916eb2a5ccd11
Shopnil Rahman <shopnil.rhm@gmail.com>
2026-05-15 00:24:57 +1200

    Base Code

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,52 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      {/* Title */}
+      <h1 className="text-2xl font-medium text tracking-tight mb-1">
+        BeeSwarm<span className="text-amber-600">.Live</span>
+      </h1>
+
+      <span className="text-5xl text-amber-900 font-bold mb-0 leading-0.8">
+        Bee Swarm Simulator's
+      </span>  <span className="text-5xl bg-linear-to-r from-amber-600 to-yellow-300 bg-clip-text text-transparent font-bold italic mb-15 leading-0.9">
+        Ultimate Tool {" "}
+      </span>
+
+      <div className="flex gap-4 justify-center">
+        <button className="btn btn-primary btn-lg rounded-full px-8 shadow-lg hover:scale-110 transition-transform leading-tight">
+          Find a Server
+        </button>
+        <button className="btn btn-ghost btn-lg rounded-full px-8 text-amber-900 hover:scale-110 hover:bg-amber-200 transition-transform">
+          Documentation
+        </button>
+      </div>
+
+      {/* Stats Grid */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-8 w-full max-w-4xl">
+        {[
+          { label: 'Active Players', value: '1,284', icon: '📍' },
+          { label: 'Honey Produced', value: '42.8 GB', icon: '🍯' },
+          { label: 'Disocrd Servers', value: '30+', icon: '✨' }
+        ].map((stat, i) => (
+          <div key={i} className="stats shadow  border-2   border-amber-600/20 bg-white/60 backdrop-blur-md rounded-2xl">
+            <div className="stat">
+              <div className="stat-figure text-amber-600 text-2xl">{stat.icon}</div>
+              <div className="stat-title text-amber-900/60 font-bold">{stat.label}</div>
+              <div className="stat-value text-amber-700">{stat.value}</div>
+            </div>
+          </div>
+        ))}
+
+        {/*Google Authentication Button TODO*/}
+        <div className="absolute top-5 right-10 z-50">
+          <button className="btn rounded-xl bg-white text-black border-2 border-amber-600">
+            <svg aria-label="Google logo" width="16" height="16" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+            Sign in with Google
+          </button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
